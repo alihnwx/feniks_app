ef5b24790c1477fee9fffc253dbac58793787038
Alikhan <164726833+alihnwx@users.noreply.github.com>
2026-06-19 14:47:25 +0500

    Added mail and phone number to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,8 @@ export default function Footer() {
       <div className="wrap footer-row">
         <div className="logo-text">Feniks Logistics</div>
         <div>© 2026 Feniks Logistics. Международные грузоперевозки. г. Алматы.</div>
+        <div>feniks@fenikslogistic.kz</div>
+        <div>+7 (771) 544 19 94</div>
       </div>
     </footer>
   );
