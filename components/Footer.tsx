280d0908a1bbf58b7a0fcbc1ffe213ac67e1e092
Alikhan <164726833+alihnwx@users.noreply.github.com>
2026-06-22 17:46:42 +0500

    Changed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <footer>
       <div className="wrap footer-row">
-        <Logo />
         <div>© 2026 Feniks Logistics. Международные грузоперевозки. г. Алматы.</div>
         <div>feniks@fenikslogistic.kz</div>
         <div>+7 (771) 544 19 94</div>
