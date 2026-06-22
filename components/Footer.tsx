12df646a5763c9fc31beb6dee46cfa9e0476c913
Alikhan <164726833+alihnwx@users.noreply.github.com>
2026-06-22 15:18:08 +0500

    Changed footer, add logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
+import { Logo } from "@/components/ui/Icons";
+
 export default function Footer() {
   return (
     <footer>
       <div className="wrap footer-row">
-        <div className="logo-text">Feniks Logistics</div>
+        <Logo />
         <div>© 2026 Feniks Logistics. Международные грузоперевозки. г. Алматы.</div>
         <div>feniks@fenikslogistic.kz</div>
         <div>+7 (771) 544 19 94</div>
